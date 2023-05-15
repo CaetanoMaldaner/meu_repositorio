ef28d15e12fc0f8b06321cf65960ebc4361ffce6
CaetanoMaldaner <129125325+CaetanoMaldaner@users.noreply.github.com>
2023-05-15 18:41:49 -0300

    Delete 15_05 directory

--- 15_05/ATV novas/Estruturas/projeto/js/script.js
+++
@@ -1,9 +0,0 @@
-//BOTÃO PARA ALTERAR A MSG DO H1
-function alterarH1(msg){
-    document.getElementById('msg').innerHTML = msg; 
-}
-//BOTÃO PARA ALTERAR A COR E TAMANHO DA MSG DO H1
-function alterarCorTamanho(){
-    document.getElementById('msg').style.color = "blue"
-    document.getElementById('msg').style.fontSize = "30px"
-}
